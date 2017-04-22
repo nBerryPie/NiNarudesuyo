edd51308d62c854dd616728ef2aea3e5ae1f1a78
Nagisberry <nagisberry@gmail.com>
2017-04-22 16:41:26 +0900

    Delete: 標準出入力の文字コードを指定してたけどうまく機能してなかった部分を削除

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 # coding: utf-8
-from io import TextIOWrapper
-from sys import stdin, stdout, stderr
 from nbot import NBot
 
 if __name__ == "__main__":
-    stdin = TextIOWrapper(stdin.buffer, encoding='utf-8')
-    stdout = TextIOWrapper(stdout.buffer, encoding='utf-8')
-    stderr = TextIOWrapper(stderr.buffer, encoding='utf-8')
     bot = NBot()
     bot.run()
 
