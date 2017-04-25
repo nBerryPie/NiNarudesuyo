4a628e9f354ca9fb792debf5f8bbd87ef86464a4
Nagisberry <nagisberry@gmail.com>
2017-04-25 22:51:04 +0900

    Update: execute_taskコマンドの追加

--- nbot/command.py
+++ nbot/command.py
@@ -6,3 +6,16 @@ from nbot import bot
 @bot.command_task("commands", name="commands_command")
 def on_commands_command(_: List[str]):
     print("Command List: {}".format(", ".join(bot.plugin_manager.commands)))
+
+
+@bot.command_task("execute_task", name="execute_task_command")
+def on_execute_task_command(args: List[str]):
+    print(args)
+    if len(args) > 0:
+        task = bot.plugin_manager.get_schedule_task(args[0])
+        if task is None:
+            print("Taskが見つかりません")
+        else:
+            task.task()
+    else:
+        print("引数が足りません")
